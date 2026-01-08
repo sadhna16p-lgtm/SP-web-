1cc7b57ca77093050d90427d044fcc35bbe57093
sadhna16p-lgtm <sadhna16p@gmail.com>
2026-01-08 19:03:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+Function showmsg(){
+alert("wellcom to SP web studio!");
+} 
+alert("JS connected");
+let name=sadhna;
+let age=20;
+Function hideshow(){
+    let p= document.getElementById("para");
+    if (p.style.display === "none"){
+        p.style.display== "block"
+    } else {
+        p.style.display="none";
+    }
+}
